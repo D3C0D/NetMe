7f4723e14ba1d4ad1d1e40cea55bcd7994afe490
Isaac-maker <65924751+Isaac-maker@users.noreply.github.com>
2021-04-14 00:54:51 -0600

    part-2

---
+++ NetMe/app/src/main/java/com/d3c0d/netme/SearchableActivity.java
@@ -0,0 +1,13 @@
+package com.d3c0d.netme;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
